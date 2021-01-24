3c7b1b0b845ccdcf7a3969d3c29d40ec863807de
kwak <kwakkj112@naver.com>
2021-01-24 15:46:07 +0900

    test: LiveResponseDemo Test
    currencylayer에서 제공하는 API 연동 테스트

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+import com.example.demo.currencylayer.LiveResponseDemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void liveResponseDemoTest() throws IOException {
+        LiveResponseDemo.sendLiveRequest();
+        LiveResponseDemo.httpClient.close();
     }
 
 }
